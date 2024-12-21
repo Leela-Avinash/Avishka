b976319b79099cbe184894a7982c43cb041693aa
Leela-Avinash <leelaavinash24@gmail.com>
2024-12-21 15:29:48 +0530

    changes to be made

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,26 @@ import Footer from "./components/Footer";
 function App() {
     return (
         <div className="dark:bg-gradient-to-br dark:from-gray-900 dark:via-black dark:to-gray-800 bg-[#f5f6f7]">
-            <Header />
+            {/* <Header />
             <Navbar />
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/:theme" element={<ThemePage />} />
             </Routes>
-            <Footer />
+            <Footer /> */}
+            <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-gray-900 via-black to-gray-800 text-center text-white">
+                <div>
+                    <h1 className="text-4xl md:text-6xl font-bold mb-4">
+                        Website Under Maintenance
+                    </h1>
+                    <p className="text-lg md:text-xl mb-8">
+                        We’ll be back soon! Thank you for your patience.
+                    </p>
+                    <p className="text-sm md:text-md text-gray-400">
+                        Sorry for the inconvenience caused.
+                    </p>
+                </div>
+            </div>
         </div>
     );
 }
